bc8d67266a21337bb595342b61d234e09dc3be5c
lucarv <lucarv@microsoft.com>
2020-07-01 09:32:45 +0200

    added ENV for file name

--- modules/TagManager/routes/index.js
+++ modules/TagManager/routes/index.js
@@ -6,137 +6,141 @@ const router = express.Router();
 
 var servers = [];
 var newEndpoint = {};
-console.log(newEndpoint)
 var newTags = [];
-var servers = []
-var file = '/appdata/pn.json'
-jf.readFile(file, function (err, obj) {
-  if (err) console.error(err)
-  servers = obj
-  console.log(servers)
-})
+var servers = [];
 
+var file = process.env.PNFILE || '/appdata/pn.json'
+jf.readFile(file, function (err, obj) {
+	if (err) {
+		console.error(err);
+		jf.writeFile(file, servers, function (err) {
+			if (err) console.error(err);
+			
+		});
+	} else {
+		servers = obj;
+	}
+});
+
+/*
 const Transport = require('azure-iot-device-mqtt').Mqtt;
 const Client = require('azure-iot-device').ModuleClient;
 const Message = require('azure-iot-device').Message;
 
-
 Client.fromEnvironment(Transport, function (err, client) {
-  if (err) {
-    throw err;
-  } else {
-    client.on('error', function (err) {
-      throw err;
-    });
+	if (err) {
+		throw err;
+	} else {
+		client.on('error', function (err) {
+			throw err;
+		});
 
-    // connect to the Edge instance
-    client.open(function (err) {
-      if (err) {
-        throw err;
-      } else {
-        console.log('IoT Hub module client initialized');
+		// connect to the Edge instance
+		client.open(function (err) {
+			if (err) {
+				throw err;
+			} else {
+				('IoT Hub module client initialized');
 
-        // Act on input messages to the module.
-        client.on('inputMessage', function (inputName, msg) {
-          pipeMessage(client, inputName, msg);
-        });
-      }
-    });
-  }
+				// Act on input messages to the module.
+				client.on('inputMessage', function (inputName, msg) {
+					pipeMessage(client, inputName, msg);
+				});
+			}
+		});
+	}
 });
-
-
-
+*/
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  let endpoints = [];
-  
-  console.log(servers);
+	let endpoints = [];
+
 	for (var i = 0; i < servers.length; i++) {
-    endpoints.push({ id: i, url: servers[i].EndpointUrl });
-  }
-  console.log(endpoints)
+		endpoints.push({ id: i, url: servers[i].EndpointUrl });
+	}
 
 	res.render('servers', {
 		title: 'OPCUA Tag Manager',
-		endpoints,
+    endpoints
 	});
 });
 
 router.post('/list', function (req, res, next) {
-	let endpoint = servers[req.body.id];
 	if (req.body.id == null) {
 		res.render('error', {
 			title: 'OPCUA Tag Manager',
 			message: 'No Server Selected',
 		});
 	} else {
-		let tags = endpoint.OpcNodes;
+    let endpoint = servers[req.body.id];
+    let tags = endpoint.OpcNodes;
+    let url = endpoint.EndpointUrl
 		res.render('tags', {
 			title: 'OPCUA Tag Manager',
-			tags,
+      tags,
+      url
 		});
 	}
 });
 
 router.get('/addep', function (req, res, next) {
 	res.render('addep', {
-		title: 'OPCUA Tag Manager'
+		title: 'OPCUA Tag Manager',
 	});
 });
 
-router.post('/addep', function (req, res, next) {
-  let sec = req.body.UserSecurity
+router.post('/addendp', function (req, res, next) {
+	let sec = req.body.UserSecurity;
 
-  newEndpoint['EndpointUrl'] = req.body.EndPointUrl;
-  if (sec === "true") {
-    newEndpoint['UseSecurity'] = true;
-    newEndpoint['OpcAuthenticationUsername'] = req.body.OpcAuthenticationUsername;
-    newEndpoint['OpcAuthenticationPassword'] = req.body.OpcAuthenticationPassword;
-  } else {
-    newEndpoint['UseSecurity'] = false;
-  }
-  console.log(newEndpoint)
+	newEndpoint['EndpointUrl'] = req.body.EndPointUrl;
+	if (sec === 'true') {
+		newEndpoint['UseSecurity'] = true;
+		newEndpoint['OpcAuthenticationMode'] = 'UsernamePassword';
+		newEndpoint['OpcAuthenticationUsername'] = req.body.OpcAuthenticationUsername;
+		newEndpoint['OpcAuthenticationPassword'] = req.body.OpcAuthenticationPassword;
+	} else {
+		newEndpoint['UseSecurity'] = false;
+	}
 	res.render('addtag', {
-		title: 'OPCUA Tag Manager'
-	});
-});
-
-router.get('/addtag', function (req, res, next) {
-  console.log(req.body)
-	res.render('addtag', {
-		title: 'OPCUA Tag Manager'
+		title: 'OPCUA Tag Manager',
 	});
 });
 
 router.post('/addtag', function (req, res, next) {
-  newTags.push(req.body)
+	newTags.push(req.body);
 	res.render('addtag', {
-		title: 'OPCUA Tag Manager'
+		title: 'OPCUA Tag Manager',
 	});
 });
 
-router.post('/committags', function (req, res, next) {
-  newEndpoint['OpcNodes'] = newTags;
+router.post('/committ', function (req, res, next) {
+	newEndpoint['OpcNodes'] = newTags;
+	servers.push(newEndpoint);
+	let endpoints = [];
 
-  servers.push(newEndpoint)
-  console.log(servers);
+	for (var i = 0; i < servers.length; i++) {
+		endpoints.push({ id: i, url: servers[i].EndpointUrl });
+	}
 
-  jf.writeFile(file, servers, function (err) {
-    if (err) console.error(err)
-    else {
-      res.render('addep', {
-        title: 'OPCUA Tag Manager'
-      });
-    }
-  })
+	jf.writeFile(file, servers, function (err) {
+		if (err) console.error(err);
+		else {
+			res.render('servers', {
+				title: 'OPCUA Tag Manager',
+        endpoints
+			});
+		}
+	});
 });
 
 router.post('/delete', function (req, res, next) {
 	servers.splice(req.body.id, 1);
-	let endpoints = [];
-	for (var i = 0; i < servers.length; i++) endpoints.push({ id: i, url: servers[i].EndpointUrl });
+
+  jf.writeFile(file, servers, function (err) {
+    if (err) console.error(err);
+    
+  });
 
 	res.render('servers', {
 		title: 'OPCUA Tag Manager',
